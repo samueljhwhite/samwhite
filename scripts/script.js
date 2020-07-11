1687cbaf2bdf1f12a5a5d41188fb91b4f228d327
Sam White <samueljhwhite@gmail.com>
2020-07-11 20:39:47 +0200

    v1
    v1 build of portfolio

---
+++ scripts/script.js
@@ -0,0 +1,15 @@
+// Selectors
+// const nav = document.querySelector('#navigation');
+
+// // Nav config
+// const navTop = nav.offsetTop;
+
+// function stickyNavigation() {
+//     if (window.scrollY >= navTop) {
+//         nav.classList.add('fixed-nav');
+//     } else {
+//         nav.classList.remove('fixed-nav');
+//     }
+// }
+
+// window.addEventListener('scroll', stickyNavigation);
